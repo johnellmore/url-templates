c64f1e70f66203c31c0a00d016830acc105ced2e
John Ellmore <jnellmore@gmail.com>
2021-04-26 14:52:57 -0500

    basic working implementation

--- mod.test.ts
+++ mod.test.ts
@@ -1,7 +1,28 @@
-import { url } from "./mod.ts";
+import { url, unsafeComponent, withBase } from "./mod.ts";
 import { assertEquals } from "https://deno.land/std@0.95.0/testing/asserts.ts";
 
-Deno.test("simple URL with no parts can be returned", () => {
+Deno.test("URL with no parts can be returned", () => {
   const x = url`http://example.com/`;
   assertEquals(x.toString(), "http://example.com/");
+});
+
+Deno.test("URL with interpolated path segments is correctly assembled", () => {
+  const x = url`http://example.com/a-number/${1337}/a-string/${"leet"}/`;
+  assertEquals(x.toString(), "http://example.com/a-number/1337/a-string/leet/");
+});
+
+Deno.test("URL with unsafe path is escaped during assembly", () => {
+  const x = url`http://example.com/first/${'../evil'}/third`;
+  assertEquals(x.toString(), 'http://example.com/first/..%2Fevil/third');
+});
+
+Deno.test("URL with marked unsafe component is assembled untouched", () => {
+  const x = url`http://example.com/first/${unsafeComponent('../trusted')}/third`;
+  assertEquals(x.toString(), 'http://example.com/trusted/third');
+});
+
+Deno.test("URL with marked unsafe component is assembled untouched", () => {
+  const baseUrl = withBase("http://example.com/");
+  const x = baseUrl`/first/${1337}/third`;
+  assertEquals(x.toString(), 'http://example.com/first/1337/third');
 });
--- mod.ts
+++ mod.ts
@@ -1,3 +1,41 @@
+function stringify(component: unknown): string {
+  if (component instanceof UnsafeUrlComponent) {
+    return '' + component.component;
+  } else {
+    return encodeURIComponent('' + component);
+  }
+}
+
+function assembleSegments(strings: TemplateStringsArray, vars: unknown[]): string {
+  const parts: string[] = [];
+  strings.forEach((str, index) => {
+    parts.push(str);
+    const nextVar = vars[index];
+    if (nextVar !== undefined) {
+      parts.push(stringify(nextVar));
+    }
+  });
+  return parts.join('');
+}
+
+class UnsafeUrlComponent {
+  constructor(
+    readonly component: unknown
+  ) {}
+}
+
+export function unsafeComponent(component: unknown): UnsafeUrlComponent {
+  return new UnsafeUrlComponent(component);
+}
+
 export function url(strings: TemplateStringsArray, ...vars: unknown[]): URL {
-    return new URL(strings[0]);
+  const contents = assembleSegments(strings, vars);
+  return new URL(contents);
+}
+
+export function withBase(baseUrl: string) {
+  return (strings: TemplateStringsArray, ...vars: unknown[]): URL => {
+    const contents = assembleSegments(strings, vars);
+    return new URL(contents, baseUrl);
+  }
 }
